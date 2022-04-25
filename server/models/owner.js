cf2f48ff97a363393082adc893e9fe9822fed326
Loghin01 <92971845+Loghin01@users.noreply.github.com>
2022-04-25 10:34:49 +0200

    yey

--- server/models/owner.js
+++ server/models/owner.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Owner extends Model {
     /**
@@ -12,35 +10,38 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-  };
-  Owner.init({
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  }
+  Owner.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      siret: {
+        type: DataTypes.STRING,
+      },
+      document: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    firstname:  {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    phone:  {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    siret:  {
-      type: DataTypes.STRING,
-    },
-    document: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  }, {
-    sequelize,
-    modelName: 'Owner',
-    tableName: 'owners',
-  });
+    {
+      sequelize,
+      modelName: "Owner",
+      tableName: "owners",
+    }
+  );
   return Owner;
 };
