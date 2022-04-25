16f2fce94941161d1d3422f9d5dd137da9af9088
Louis Lecouturier <71454059+LouisLecouturier@users.noreply.github.com>
2022-04-25 10:19:37 +0200

    yey

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Searchbar from "../../components/client/Searchbar/Searchbar";
+import MainMenu from "../../components/shared/Header/MainMenu/MainMenu";
+import { ReactComponent as Spinner } from "../../assets/icons/spin.svg";
+
+import { getIcon } from "../../components/shared/Pin/getIcon";
+
+import styles from "./Home.module.scss";
+
+const Home = () => {
+  const [position, setPosition] = useState(null);
+  const [isGeoLocAllowed, setIsGeoLocAllowed] = useState(true);
+
+  navigator.permissions.query({ name: "geolocation" }).then(function (result) {
+    // Will return ['granted', 'prompt', 'denied']
+    result.state === "granted" || result.state === "prompt"
+      ? setIsGeoLocAllowed(true)
+      : setIsGeoLocAllowed(false);
+  });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((geoloc) =>
+      setPosition([geoloc.coords.latitude, geoloc.coords.longitude])
+    );
+  }, []);
+
+  return (
+    <>
+      {isGeoLocAllowed && position && (
+        <div className={styles.Home}>
+          <main className={styles.mainContent}>
+            <Searchbar />
+
+            <MapContainer className={styles.map} center={position} zoom={13}>
+              <TileLayer
+                attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+                url="https://api.mapbox.com/styles/v1/louislecout/ckzwfj8rc00a414jydquyrqpv/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibG91aXNsZWNvdXQiLCJhIjoiY2t6d2ZubTEzMmoxNTJ3cGU5eGJ5amg4eiJ9.Av7gubKDgQ_33XWfC5nUHQ"
+              />
+
+              <Marker position={position} icon={getIcon("theater")}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </main>
+          <MainMenu />
+        </div>
+      )}
+
+      {!isGeoLocAllowed && !position && (
+        <div className={styles.message}>
+          <Spinner className={styles.spinner} />
+        </div>
+      )}
+      {/* {!isGeoLocAllowed && (
+        <div className={styles.message}>
+          MyTravelBuddy a besoin d'accéder à votre position pour fonctionner
+        </div>
+      )} */}
+    </>
+  );
+};
+
+export default Home;
