eadf3720eb0862a40a131d5f7ec990d5563e4b04
Mael Monteil <monteil.m.t@gmail.com>
2022-05-10 17:17:35 +0200

    apicall

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -1,111 +1,157 @@
-
 const { Event } = require("../models");
+const axios = require("axios");
 
 const test = async (req, res) => {
   console.log("yee");
   return res.status(200).send("It worksss");
 };
 
-        
-const apicall = async (req, res) => {
-  const url = `https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=evenements-publics-openagenda0&q=mus%C3%A9e&rows=550&sort=date_end&facet=tags&facet=placename&facet=department&facet=region&facet=city&facet=date_start&facet=date_end&facet=pricing_info&facet=updated_at&facet=city_district&exclude.date_end=2021`;
+const apicall = async () => {
+  //const url = `https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=evenements-publics-openagenda0&q=mus%C3%A9e&rows=10&sort=date_end&facet=tags&facet=placename&facet=department&facet=region&facet=city&facet=date_start&facet=date_end&facet=pricing_info&facet=updated_at&facet=city_district&exclude.date_end=2021`;
+  try {
+    const url = `https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=evenements-publics-openagenda0&q=mus%C3%A9e&rows=5&sort=date_end&exclude.date_end=2021`;
+    let reponse = await axios.get(url).then((res) => {
+      return res.data;
+    });
 
- try{
-    fetch(url, {
-      method: 'POST',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
-          'Content-Type': 'application/json'
-      }
-    })
-    res.status(200).send("It worksss")
-    .then(res => res.json())
-    .then(response => console.log(res.json, response))
-    
-    
+    return reponse;
+  } catch (err) {
+    console.log(err);
+    return err;
   }
-  catch(err){
-    console.log(err)
-    return res.status(500).json(err)
-  }
+};
 
+const ApiRegister = async (req, res) => {
+  try {
+    let json = await apicall();
+    //var json = JSON.stringify(data)
+    //var json = JSON.parse(data);
+    var i = 0;
+    nhits = 5;
+   
+    //let events = [];
+
+    //console.log(json.records);
+
+    // console.log(json.records[i].fields)
+    for (let i = 0; i < nhits; i++) {
+      let eventData = {
+        //location_id: i,
+        name: json.records[i].fields.title,
+        category: json.records[i].fields.pricing_info,
+        image: json.records[i].fields.image,
+        description: json.records[i].fields.free_text,
+        start_datetime: json.records[i].fields.date_start,
+        end_datetime: json.records[i].fields.date_end,
+        permanent: 0,
+      };
+
+      
+      const event = await Event.create({
+        ...eventData
+      });
+      event.addLocation()
+      //await Event.create({...event});
+    }
+
+    events = Event.findAll();
+
+    
+    console.log(events);
+    return res.json(events);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
 };
 
 
-// let data = fetch(url)
-//   .then(async (res)=>{
-//       let data = await res.json();
-//       console.log(res.json());
-
-//       return res.json;
-//   })
-
-// .then(function (data){
-//       appendData(data);
-//   })
-
-// .catch(function (err) {
-//   });
 
 
-const register = async (req,res) => {
-  const {location_id, name, category, image, description, rate, start_datetime, end_datetime, permanent} = req.body
 
-  try{
-    const event = await Event.create({location_id, name, category, image, description, rate, start_datetime, end_datetime, permanent})
-  
-    return res.json(event)
-  }catch(err){
-    console.log(err)
-    return res.status(500).json(err)
+const register = async (req, res) => {
+  const {
+    location_id,
+    name,
+    category,
+    image,
+    description,
+    rate,
+    start_datetime,
+    end_datetime,
+    permanent,
+  } = req.body;
+  try {
+    const event = await Event.create({
+      location_id,
+      name,
+      category,
+      image,
+      description,
+      rate,
+      start_datetime,
+      end_datetime,
+      permanent,
+    });
+
+    return res.json(event);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
   }
-}
+};
 
 
-const getById = async (req,res) => {
-  const id = req.params.id
+const getById = async (req, res) => {
+  const id = req.params.id;
   try {
     const event = await Event.findOne({
       where: { id },
-    })
+    });
 
-    return res.json(event)
+    return res.json(event);
   } catch (err) {
-    console.log(err)
-    return res.status(500).json({error: 'something went wrong'})
+    console.log(err);
+    return res.status(500).json({ error: "something went wrong" });
   }
-}
+};
 
-const deleteById = async (req,res) => {
-  const id = req.params.id
+const deleteById = async (req, res) => {
+  const id = req.params.id;
   try {
-    const event = await Event.findOne({ where: { id }})
-    await event.destroy()
+    const event = await Event.findOne({ where: { id } });
+    await event.destroy();
 
-    return res.json({message: 'Event deleted'})
-    } catch (err) {
-    console.log(err)
-    return res.status(500).json({error: 'something went wrong'})
+    return res.json({ message: "Event deleted" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "something went wrong" });
   }
-}
+};
 
-const modifyById = async (req,res) => {
-  const id = req.params.id
-  const {name, category, image, description } = req.body
+const modifyById = async (req, res) => {
+  const id = req.params.id;
+  const { name, category, image, description } = req.body;
   try {
-    const event = await Event.findOne({ where: { id }})
-    event.name = name
-    event.category = category
-    event.image = image
-    event.description = description
+    const event = await Event.findOne({ where: { id } });
+    event.name = name;
+    event.category = category;
+    event.image = image;
+    event.description = description;
 
-    await event.save()
+    await event.save();
 
-    return res.json(event)
-    } catch (err) {
-    console.log(err)
-    return res.status(500).json({error: 'something went wrong'})
+    return res.json(event);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "something went wrong" });
   }
-}
-module.exports = { test, register, getById, deleteById, modifyById, apicall };
+};
+module.exports = {
+  test,
+  ApiRegister,
+  register,
+  getById,
+  deleteById,
+  modifyById,
+};
